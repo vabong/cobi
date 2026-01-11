7c34bd4be490f4fd81afb77a1c4e6f1c740b6e2d
vabong <vantondc@tcd.ie>
2026-01-11 03:38:17 +0100

    Update site and base URL in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,2 +1,6 @@
 import { defineConfig } from 'astro/config';
-export default defineConfig({ site: 'https://example.com' });
+
+export default defineConfig({
+  site: 'https://vabong.github.io/cobi/',
+  base: '/cobi/',
+});
